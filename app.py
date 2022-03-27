ba104bf5c8cb19ea0e92e482dd6b8204e6071e73
ho-tony <ho.tony787@gmail.com>
2022-03-27 14:55:15 -0400

    added very barebones script to calculate stock correlation

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
-import datetime
+#%%
+from datetime import datetime
+import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader.data as pdr
 import numpy as np
 
+start = datetime(2020, 3, 31)
+end = datetime(2022, 12, 31)
 
-date_from = datetime.date(2018, 1, 1)
-date_to = datetime.date(2022, 12, 31)
-tickerlist = ['AAPL']
+FIRST_TICK = 'HMC'
+SECOND_TICK = 'TSLA'
 
-data = pdr.DataReader(name="AAPL", data_source='yahoo', start=date_from, end= date_to)
+stockOne = pdr.DataReader(FIRST_TICK, 'yahoo', start, end)
+
+stockTwo = pdr.DataReader(SECOND_TICK, 'yahoo', start, end)
+
+# plots out close price of a stock
+
+stockOne_adj = np.array(stockOne['Close'])
+stockTwo_adj = np.array(stockTwo['Close'])
+
+corr_matrix = np.corrcoef(stockOne_adj, stockTwo_adj)
+corr = corr_matrix[0,1]
+R_sq = corr**2
+print(R_sq)
+
+
+plt.figure(dpi=1200)
+
+stockOne['Adj Close'].plot(legend=True, figsize=(15, 15), label = FIRST_TICK)
+
+stockTwo['Adj Close'].plot(legend=True, figsize=(15, 15), label = SECOND_TICK)
 
 
 
-print(data)
-# print("hello world")
+
+
+# print(corr_matrix)
+
+
+
+# %%
