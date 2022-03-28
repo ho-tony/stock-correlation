0df794b600d3b9c7c4e3477cc9a34807cfdb9aaf
ho-tony <ho.tony787@gmail.com>
2022-03-28 11:26:53 -0400

    implemented stock info output

--- app.py
+++ app.py
@@ -3,13 +3,24 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import pandas_datareader.data as pdr
+import yahoo_fin.stock_info as si
 import numpy as np
+import json 
 
 start = datetime(2020, 3, 31)
 end = datetime(2022, 12, 31)
 
-FIRST_TICK = 'HMC'
-SECOND_TICK = 'TSLA'
+FIRST_TICK = 'QQQ'
+SECOND_TICK = 'ARKK'
+print(FIRST_TICK)
+print("-----------------------------------------------")
+print(json.dumps(si.get_quote_table(FIRST_TICK),indent=4, sort_keys=True))
+print(SECOND_TICK)
+print("-----------------------------------------------")
+print(json.dumps(si.get_quote_table(SECOND_TICK),indent=4, sort_keys=True))
+
+
+
 
 stockOne = pdr.DataReader(FIRST_TICK, 'yahoo', start, end)
 
@@ -23,10 +34,13 @@ stockTwo_adj = np.array(stockTwo['Close'])
 corr_matrix = np.corrcoef(stockOne_adj, stockTwo_adj)
 corr = corr_matrix[0,1]
 R_sq = corr**2
-print(R_sq)
+print ("R = ",R_sq**0.5)
+print("R2 = ",R_sq)
 
 
-plt.figure(dpi=1200)
+plt.figure(dpi=400)
+
+
 
 stockOne['Adj Close'].plot(legend=True, figsize=(15, 15), label = FIRST_TICK)
 
